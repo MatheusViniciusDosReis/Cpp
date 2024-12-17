7092e328268daff958b61b79e407e8b2e4e5a467
Matheus Reis <152439030+MatheusViniciusDosReis@users.noreply.github.com>
2024-12-16 21:19:49 -0300

    Update Exemplo.cpp

--- Operador Ternário/Exemplo | Exercício/Exemplo.cpp
+++ Operador Ternário/Exemplo | Exercício/Exemplo.cpp
@@ -1,1 +1,32 @@
-exemplo
+// EXEMPLO DE OPERADOR TERNÁRIO
+// PROGRAMA DE CALCULAR NOTA
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//declarando as variasveis que irão receber a notas
+	int nota1, nota2, notaFinal;
+	string resultado;
+
+	//guardando a nota 1
+	cout << "Digite a PRIMEIRA nota: ";
+	cin >> nota1;
+
+
+	//guardando a nota 2
+	cout << "Digte a SEGUNDA nota: ";
+	cin >> nota2;
+
+	//obtendo a nota final
+	notaFinal = nota1 + nota2;
+
+	//INÍCIO DO OPERADOR TERNÁRIO
+	(notaFinal >= 60) ? resultado = "APROVADO" : resultado = "REPROVADO";
+
+	//Informando a situação do aluno
+	cout << "\nO aluno está " << resultado << endl;
+
+	return 0;
+}
