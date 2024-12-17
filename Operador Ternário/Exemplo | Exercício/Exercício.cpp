ff86fb7ce821d49a85edfdebbffb27b1f0828111
Matheus Reis <152439030+MatheusViniciusDosReis@users.noreply.github.com>
2024-12-16 21:43:40 -0300

    Create Exercício.cpp

---
+++ Operador Ternário/Exemplo | Exercício/Exercício.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int entrada1, entrada2;
+	bool condicao;
+
+	cout << "Digite o primeiro numero: ";
+	cin >> entrada1;
+	cout << "Digite o segundo numero: ";
+	cin >> entrada2;
+
+	if(entrada2 != 0) {     //evitando a divisao por zero
+		(entrada1 % entrada2 == 0) ? condicao = true : condicao = false;
+	}
+	else {
+		cout << "Divisao por zero nao eh permitida" << endl;
+	}
+
+	if (condicao == true) {
+		cout << entrada1 << " eh mutiplo de " << entrada2 << endl;
+	}
+	else {
+		cout << entrada1 << " nao eh mutiplo de " << entrada2 << endl;
+	}
+
+	return 0;
+}
